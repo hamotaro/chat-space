2b344b24a6eb8e7b5eb03e8aff3a7b27c617ca8b
hamotaro <tetsuya1329@gmail.com>
2019-11-09 14:31:05 +0900

    a

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,12 +1,13 @@
 class Api::MessagesController < ApplicationController
-  skip_before_action :index, only: users
+  
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
-    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+    @messages = group.messages.includes(:user).where('id > ?', params[:id])
     respond_to do |format| 
       format.html 
       format.json
     end
   end
 end
+#"id > #{last_message_id}"
